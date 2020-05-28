3cdfa86780a7b84b118569161860ebecc1a7b9a5
Jason Blocklove <jason.blocklove@gmail.com>
2020-05-28 00:06:16 -0400

    added bstack

--- dwm.c
+++ dwm.c
@@ -2480,9 +2480,10 @@ main(int argc, char *argv[])
 	return EXIT_SUCCESS;
 }
 
+/*
 static void
 bstack(Monitor *m) {
-	int w, h, mh, mx, tx, ty, tw;
+	int w, h, mh, mx, tx, ty, tw, oe = enablegaps, ie = enablegaps;
 	unsigned int i, n;
 	Client *c;
 
@@ -2510,6 +2511,50 @@ bstack(Monitor *m) {
 				tx += WIDTH(c);
 		}
 	}
+}
+*/
+
+void
+bstack(Monitor *m)
+{
+	unsigned int i, n, w, mh, mx, tx, ns, oe = enablegaps, ie=enablegaps;
+	Client *c;
+
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	if (n == 0)
+		return;
+
+	if (smartgaps == n){
+		oe = 0;
+	}
+
+	if(n == 1){
+		c = nexttiled(m->clients);
+		resize(c, m->wx + m->gappov*oe, m->wy + m->gappoh*oe, m->ww - 2 * c->bw - 2*m->gappov*oe, m->wh - 2 * c->bw - 2 * m->gappoh*oe, 0);
+		return;
+	}
+
+	if (n > m->nmaster){
+		mh = m->nmaster ? m->wh * m->mfact : m->gappov*oe;
+		ns = 2;
+	}
+	else{
+		mh = m->wh;
+		ns = 1;
+	}
+	for (i = 0, mx = tx = m->gappoh*oe, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+		if (i < m->nmaster) {
+			w = (m->ww - mx) / (MIN(n, m->nmaster) - i) - m->gappiv*ie;
+			resize(c, m->wx + mx, m->wy + m->gappih*ie, w - 2*c->bw, mh - 2*c->bw - (m->gappih*ie)*(5-ns)/2, 0);
+			mx += WIDTH(c) + m->gappoh*oe;
+		} else {
+			w = (m->ww - tx) / (n - i) - m->gappiv*ie;
+			if(m->nmaster == 0)
+				resize(c, m->wx + tx, m->wy + mh, w - (2*c->bw), m->wh - mh - 2*c->bw - m->gappih*ie, False);
+			else
+				resize(c, m->wx + tx, m->wy + mh + (m->gappih*ie)/ns, w - (2*c->bw), m->wh - mh - 2*c->bw - (m->gappih*ie)*(5-ns)/2, False);
+			tx += WIDTH(c) + m->gappov*oe;
+		}
 }
 
 static void
